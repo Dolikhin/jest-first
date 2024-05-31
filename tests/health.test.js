4b57ebb39397cf2c039adec2d615dbc8c2623cd9
Dmitrii Dolikhin <haxall53@gmail.com>
2024-05-31 17:30:59 +0300

    добавил тест с использованием each

--- tests/health.test.js
+++ tests/health.test.js
@@ -1,5 +1,19 @@
 import getHealthStatus from '../src/js/health.js';
+
 test('Возвращает сообщение в зависимости от количества жизни', () => {
+  const mage = { name: 'Маг', health: 90 };
+  const expected = 'healthy';
+  const received = getHealthStatus(mage);
+  expect(received).toBe(expected);
+});
+
+test.each([
+  { name: 'Маг', health: 90 },
+  { name: 'Воин', health: 15 },
+  { name: 'Охотник', health: 45 },
+  { name: 'Некромант', health: 1 },
+])
+('Возвращает сообщение в зависимости от количества жизни', (caracter) => {
   const mage = { name: 'Маг', health: 90 };
   const expected = 'healthy';
   const received = getHealthStatus(mage);
